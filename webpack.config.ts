cd7948930981c47d895b26eaf125fc14dd723a00
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-06-21 01:54:23 +0900

    add draft code.

---
+++ webpack.config.ts
@@ -0,0 +1,29 @@
+import { ConfigurationFactory } from 'webpack'
+import path from 'path'
+
+const config: ConfigurationFactory = () => {
+  return {
+    entry: {
+      main: path.join(__dirname, 'src', 'main.ts')
+    },
+    output: {
+      // distディレクトリにmain.jsを吐く
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /.ts$/,
+          use: 'ts-loader',
+          exclude: '/node_modules/'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+  }
+}
+
+export default config
